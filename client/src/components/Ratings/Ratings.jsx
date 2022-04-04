a6a5a34a914c9c5f486b39556f3aa532d3bb63c9
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2022-04-04 11:09:09 -0700

    added styling to rendered components

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import ProductBreakdown from './ProductBreakdown.jsx';
 import HeaderRating from './HeaderRating.jsx';
+import styled from 'styled-components';
 import Distribution from './Distribution.jsx';
+
+const StyledHeaderRating = styled.p`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const StyledDistrubution = styled.p`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: orange;
+  background: papayawhip;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
+const StyledProductBreakdown = styled.p`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: LightBlue;
+  background: papayawhip;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
+
 
 const Ratings = (props) => {
   return (
     <>
-      <HeaderRating />
-      <Distribution />
-      <ProductBreakdown />
+      <StyledHeaderRating>
+        <HeaderRating />
+      </StyledHeaderRating>
+      <StyledDistrubution>
+        <Distribution />
+      </StyledDistrubution>
+      <StyledProductBreakdown>
+        <ProductBreakdown />
+      </StyledProductBreakdown>
     </>
   )
 }
