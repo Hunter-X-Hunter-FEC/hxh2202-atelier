ac3cba8b76f0f092549ed73e39a1c5d017fefa45
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2022-04-07 11:06:27 -0700

    initial commit for mini-server branch

---
+++ server/utils/apiCalls.js
@@ -0,0 +1,14 @@
+// const axios = require('axios');
+
+// var apiURL = `https://app-hrsei-api.herokuapp.com/api/fec2/rfp`
+
+// const createAxiosOptions = (method, endpoint, params) => {
+//   const options = {
+//     method: 'GET',
+//     url: apiURL + endpoint,
+//     headers: {
+//       'Authorization': `${process.env.REACT_APP_API_KEY}`
+//     },
+//     params
+//   };
+// }
